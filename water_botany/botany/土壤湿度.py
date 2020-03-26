16af7b68597126659030a184179b0b444d230583
zzsealy <1147575371@qq.com>
2020-03-27 00:10:37 +0800

    feat:

--- water_botany/botany/土壤湿度.py
+++ water_botany/botany/土壤湿度.py
@@ -4,13 +4,20 @@ import RPi.GPIO as GPIO
 import time
 
 channel = 21 #管脚40，参阅树莓派引脚图，物理引脚40对应的BCM编码为21
+channel2 = 20 # 控制继电器高低电平的引脚
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
+GPIO.setup(channel2, GPIO.IN)
+GPIO.setup(channel2, GPIO.OUT)
 
 while True:
         if GPIO.input(channel) == GPIO.LOW:
                 print "土壤检测结果：潮湿"
+                print(GPIO.input(channel2))
+                GPIO.output(channel2, GPIO.LOW)
         else:
                 print "土壤检测结果：干燥"
+                print(GPIO.input(channel2))
+                GPIO.output(channel2, GPIO.HIGH)
         time.sleep(1)
