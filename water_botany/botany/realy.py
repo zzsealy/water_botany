19afb54b915e2b7dc3a77751b4ab755d74b22382
zzsealy <1147575371@qq.com>
2020-03-27 09:05:28 +0800

    继电器

--- water_botany/botany/realy.py
+++ water_botany/botany/realy.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
 import time                
 
-print("PC ON: PC-ON")   
+print("PC ON: on")   
 print("Exit: Q and q")
  
 while True:
     user_choice=input("Choice:")
-    if user_choice=="PC-ON":        
+    if user_choice=="on":        
           GPIO.setmode(GPIO.BCM)     
-          GPIO.setup(25,GPIO.OUT)    
-          GPIO.output(25,GPIO.LOW)
+          GPIO.setup(20,GPIO.OUT)    
+          GPIO.output(20,GPIO.LOW)
           print("3")
           time.sleep(1.0)            
           print("2")
@@ -17,5 +17,8 @@ while True:
           print("1")
           time.sleep(1.0)            
           GPIO.cleanup() 
+
     elif user_choice=="q" or user_choice=="Q":      
-          GPIO.cleanup()         
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(20, GPIO.OUT)
+        GPIO.output(20, GPIO.LOW)
