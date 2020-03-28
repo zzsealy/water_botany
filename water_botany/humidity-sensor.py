871e8c381d8d6ad08f1ddea976d0ba2395dbdbed
ruiquan.dai <1147575371@qq.com>
2020-03-28 10:43:45 +0800

    style

--- water_botany/humidity-sensor.py
+++ water_botany/humidity-sensor.py
@@ -2,7 +2,21 @@
 # -*- coding: UTF-8 -*-
 import RPi.GPIO as GPIO
 import time
-from water_botany.realy import start_realy
+        
+
+
+def start_realy(num):
+    GPIO.setmode(GPIO.BCM)     
+    GPIO.setup(20,GPIO.OUT)    
+    GPIO.output(20,GPIO.LOW)
+
+    print("开始浇水")
+    GPIO.cleanup()
+    time.sleep(num)
+    GPIO.setmode(GPIO.BCM)     
+    GPIO.setup(20,GPIO.OUT)    
+    GPIO.output(20,GPIO.LOW)
+    print("浇水结束")
 
 
 channel = 21 #管脚40，参阅树莓派引脚图，物理引脚40对应的BCM编码为21
